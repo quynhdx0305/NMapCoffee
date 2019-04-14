833f2255716e5bb65576036bac427bee7060fa1d
Quynh_Do <quynhdx0305@gmail.com>
2019-04-14 15:57:53 +0700

    add library lottie

--- app/src/main/java/com/quynhdx/mapcoffee2/view/Splash.kt
+++ app/src/main/java/com/quynhdx/mapcoffee2/view/Splash.kt
@@ -1,31 +1,38 @@
 package com.quynhdx.mapcoffee2.view
 
+import android.animation.Animator
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import com.google.gson.Gson
 import com.quynhdx.mapcoffee2.R
 import com.quynhdx.mapcoffee2.model.ListCoffee_
+import kotlinx.android.synthetic.main.activity_splash.*
 
 private const val TAG = "Splash ==="
+const val NOT_YET = 0
+const val DONE = 1
 
 class Splash : AppCompatActivity() {
+
+    @Volatile private var check = NOT_YET
+    val gson = Gson()
+    var listDataCoffee : ArrayList<ListCoffee_> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        // Write a message to the database
 
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("list coffee")
-        val gson = Gson()
-        val listDataCoffee = arrayListOf<ListCoffee_>()
 
-        // Read from the database
+        playAnimation()
+        listenDataChange(myRef)
+    }
+
+    private  fun listenDataChange(myRef: DatabaseReference) {
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 val _size = dataSnapshot.children.count()
@@ -43,12 +50,40 @@ class Splash : AppCompatActivity() {
                     }
                 }
                 Log.d(TAG, listDataCoffee.size.toString())
+                check = DONE
             }
 
             override fun onCancelled(error: DatabaseError) {
                 Log.w(TAG, "Failed to read value.", error.toException())
             }
         })
+    }
 
+    private fun playAnimation() {
+
+        animation_splash.setAnimation("world-locations.json")
+
+        animation_splash.addAnimatorListener(object : Animator.AnimatorListener{
+            override fun onAnimationRepeat(p0: Animator?) {}
+
+            override fun onAnimationEnd(p0: Animator?) {
+
+                when(check) {
+                    DONE -> {
+                        startActivity(Intent(this@Splash, MapsActivity::class.java))
+                        finish()
+                    }
+                    NOT_YET -> animation_splash.playAnimation()
+                }
+
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {}
+
+            override fun onAnimationStart(p0: Animator?) {}
+
+        })
+
+        animation_splash.playAnimation()
     }
 }
